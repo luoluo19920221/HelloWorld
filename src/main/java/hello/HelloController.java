da62dda1f8aedad39f688c49172431db419c2757
luoluo19920221 <shenlijun0118@sina.com>
2015-11-23 19:19:11 +0800

    Initial commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,25 @@
+package hello;
+
+/**
+ * Created by shenlijun on 2015/11/23.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+
+@Controller
+@RequestMapping(value = "welcome")
+public class HelloController {
+
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    public  String hello(ModelMap modelMap) {
+        System.out.println("luoluo zhenbang");
+        modelMap.addAttribute("message", "spring mvc  hello world");
+        modelMap.addAttribute("luoluo", "luoluo's hello world");
+        return  "hello";
+    }
+}
